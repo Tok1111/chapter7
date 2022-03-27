cc9a94ba674a2a3c0551807f7e64e69ba649caf4
Tok1111 <96003539+Tok1111@users.noreply.github.com>
2022-03-27 19:23:50 +0800

    Update dirlister.py

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -4,10 +4,10 @@
 import os
 
 def run(**args):
-    print "[*] In dirlister module."
+    print ("[*] In dirlister module.")
     # 列出当前目录的所有文件,并作为字符串返回
     files = os.listdir(".")
     return str(files)
 if __name__ == "__main__":
-        print run()
+        print (run())
 
